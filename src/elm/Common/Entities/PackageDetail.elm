7c410ed980eec19f19b889cafe76a946c3f727aa
Jan Slifka <jan.slifka@gmail.com>
2019-06-11 15:18:22 +0200

    Add version sorting

--- src/elm/Common/Entities/PackageDetail.elm
+++ src/elm/Common/Entities/PackageDetail.elm
@@ -6,19 +6,20 @@ module Common.Entities.PackageDetail exposing
 import Common.Entities.OrganizationInfo as OrganizationInfo exposing (OrganizationInfo)
 import Json.Decode as D exposing (Decoder)
 import Json.Decode.Pipeline as D
+import Version exposing (Version)
 
 
 type alias PackageDetail =
     { id : String
     , name : String
     , kmId : String
-    , version : String
+    , version : Version
     , description : String
     , organization : OrganizationInfo
     , metamodelVersion : Int
     , parentPackageId : Maybe String
     , readme : String
-    , versions : List String
+    , versions : List Version
     }
 
 
@@ -28,10 +29,10 @@ decoder =
         |> D.required "id" D.string
         |> D.required "name" D.string
         |> D.required "kmId" D.string
-        |> D.required "version" D.string
+        |> D.required "version" Version.decoder
         |> D.required "description" D.string
         |> D.required "organization" OrganizationInfo.decoder
         |> D.required "metamodelVersion" D.int
         |> D.required "parentPackageId" (D.maybe D.string)
         |> D.required "readme" D.string
-        |> D.required "versions" (D.list D.string)
+        |> D.required "versions" (D.list Version.decoder)
